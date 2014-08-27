c6cf4328bdf3fe5f6e903d118eb6b3f975a27c58
rainyjune <rainyjune@live.cn>
2014-08-27 17:53:08 +0800

    Add simpleLoader.js

---
+++ simpleLoader.js
@@ -0,0 +1,24 @@
+﻿(function (window) {
+  if(!Array.isArray) {
+    Array.isArray = function (vArg) {
+      return Object.prototype.toString.call(vArg) === "[object Array]";
+    };
+  }
+
+  function loadJsFiles(files) {
+    if (typeof files === "string") {
+      requireScript(files);
+    } else if(Array.isArray(files)){
+      for (var i = 0, len = files.length; i < len; i++) {
+        requireScript(files[i]);
+      }
+    }
+  }
+  function requireScript(file) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = file;
+    document.body.appendChild(script);
+  }
+  window.loadJsFiles = loadJsFiles;
+})(window);
