1ad02fc19fae2e64ee6779f4a5a9f20ea7c1b423
rainyjune <rainyjune@live.cn>
2015-05-21 15:45:31 +0800

    Add demo files

---
+++ demos/js/simpleLoader1.js
@@ -0,0 +1,5 @@
+var p = document.getElementById("intro");
+var newP = document.createElement("p");
+newP.className = "scripts";
+newP.innerHTML = "simpleLoader1.js is loaded.";
+document.body.insertBefore(newP, p);
---
+++ demos/js/simpleLoader2.js
@@ -0,0 +1,5 @@
+var p = document.getElementById("intro");
+var newP = document.createElement("p");
+newP.innerHTML = "simpleLoader2.js is loaded.";
+newP.className = "scripts";
+document.body.insertBefore(newP, p);
