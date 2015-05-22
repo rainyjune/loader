ab0a2de9b92e533e06725c52a4bcf5321929c867
rainyjune <rainyjune@live.cn>
2015-05-22 11:41:12 +0800

    Update simpleLoader.js

--- simpleLoader.js
+++ simpleLoader.js
@@ -1,14 +1,46 @@
 ﻿(function (window) {
+
   "use strict";
-  function loadJsFiles(files) {
+
+  function getExtention(file) {
+    var ext = "js";
+    var arr = file.split(".");
+    if (arr.length > 1) {
+      ext = arr.pop();
+    }
+    return ext;
+  } 
+
+  function loadFile(file) {
+    var ext = getExtention(file);
+    switch (ext) {
+      case "js":
+        requireScript(file);
+        break;
+      case "css":
+        requireCSS(file);
+        break;
+      case "png":
+      case "jpg":
+      case "jpeg":
+      case "gif":
+        requireImage(file);
+        break;
+      default:
+        break;
+    }
+  }
+
+  function require(files) {
     if (typeof files === "string") {
-      requireScript(files);
+      loadFile(files);
     } else if(Array.isArray(files)){
       for (var i = 0, len = files.length; i < len; i++) {
-        requireScript(files[i]);
+        loadFile(files[i]);
       }
     }
   }
+
   function requireScript(file) {
     var script = document.createElement('script');
     script.type = 'text/javascript';
@@ -17,10 +49,20 @@
     script.defer = false;
     document.body.appendChild(script);
   }
+
+  function requireCSS(file) {
+    var head = document.getElementsByTagName("head")[0];
+    var css = document.createElement("link");
+    css.type = "text/css";
+    css.rel = "stylesheet";
+    css.href = file;
+    head.appendChild(css);
+  }
+
   if(!Array.isArray) {
     Array.isArray = function (vArg) {
       return Object.prototype.toString.call(vArg) === "[object Array]";
     };
   }
-  window.loadJsFiles = loadJsFiles;
+  window.require = require;
 })(window);
