27ec273678c4d3f0faa3f556ce3f337712d63939
rainyjune <rainyjune@live.cn>
2015-05-22 16:41:14 +0800

    Update simpleLoader.html

--- demos/js/simpleLoader2.js
+++ demos/js/simpleLoader2.js
@@ -1,5 +1,5 @@
 var p = document.getElementById("intro");
 var newP = document.createElement("p");
-newP.innerHTML = "simpleLoader2.js is loaded.";
+newP.innerHTML = "simpleLoader2.js is executed.";
 newP.className = "scripts";
 document.body.insertBefore(newP, p);
